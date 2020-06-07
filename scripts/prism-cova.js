e23a3db7a470aafb1d0b17689269b7435fe23706
NickStrupat <nick.strupat@gmail.com>
2020-06-06 22:58:43 -0400

    syntax hightlighting and nice code font with ligatures

---
+++ scripts/prism-cova.js
@@ -0,0 +1,98 @@
+Prism.languages.clike = {
+	'comment': [
+		{
+			pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true,
+			greedy: true
+		}
+	],
+	'string': {
+		pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+		greedy: true
+	},
+	'class-name': {
+		pattern: /(\b(?:class|interface|extends|implements|trait|instanceof|new)\s+|\bcatch\s+\()[\w.\\]+/i,
+		lookbehind: true,
+		inside: {
+			'punctuation': /[.\\]/
+		}
+	},
+	'keyword': /\b(?:if|else|while|do|for|return|in|instanceof|function|new|try|throw|catch|finally|null|break|continue)\b/,
+	'boolean': /\b(?:true|false)\b/,
+	'function': /\w+(?=\()/,
+	'number': /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
+	'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+	'punctuation': /[{}[\];(),.:]/
+};
+
+Prism.languages.cova = Prism.languages.extend('clike', {
+	'comment': [
+		{
+			pattern: /(^|[^\\])\(\*[\s\S]*?\*\)/,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true
+		}
+	],
+	'string': {
+		pattern: /(?:"""[\s\S]*?"""|@"(?:""|[^"])*"|"(?:\\[\s\S]|[^\\"])*")B?|'(?:[^\\']|\\(?:.|\d{3}|x[a-fA-F\d]{2}|u[a-fA-F\d]{4}|U[a-fA-F\d]{8}))'B?/,
+		greedy: true
+	},
+	'class-name': {
+		pattern: /(\b(?:exception|inherit|interface|new|of|type)\s+|\w\s*:\s*|\s:\??>\s*)[.\w]+\b(?:\s*(?:->|\*)\s*[.\w]+\b)*(?!\s*[:.])/,
+		lookbehind: true,
+		inside: {
+			'operator': /->|\*/,
+			'punctuation': /\./
+		}
+	},
+	'keyword': /\b(?:let|var|return|use|yield|foreach)(?:!\B|\b)|\b(?:abstract|and|as|assert|base|begin|class|default|delegate|do|done|downcast|downto|elif|else|end|exception|extern|false|finally|for|fun|function|global|if|in|inherit|inline|interface|internal|lazy|match|member|module|mutable|namespace|new|not|null|of|open|or|override|private|public|rec|select|static|struct|then|to|true|try|type|upcast|val|void|when|while|with|asr|land|lor|lsl|lsr|lxor|mod|sig|atomic|break|checked|component|const|constraint|constructor|continue|eager|event|external|fixed|func|include|method|mixin|object|parallel|process|protected|pure|sealed|tailcall|trait|virtual|volatile)\b/,
+	'number': [
+		/\b0x[\da-fA-F]+(?:un|lf|LF)?\b/,
+		/\b0b[01]+(?:y|uy)?\b/,
+		/(?:\b\d+\.?\d*|\B\.\d+)(?:[fm]|e[+-]?\d+)?\b/i,
+		/\b\d+(?:[IlLsy]|u[lsy]?|UL)?\b/
+	],
+	'operator': /([<>~&^])\1\1|([*.:<>&])\2|<-|->|[!=:]=|<?\|{1,3}>?|\??(?:<=|>=|<>|[-+*/%=<>])\??|[!?^&]|~[+~-]|:>|:\?>?/
+});
+Prism.languages.insertBefore('cova', 'keyword', {
+	'preprocessor': {
+		pattern: /^[^\r\n\S]*#.*/m,
+		alias: 'property',
+		inside: {
+			'directive': {
+				pattern: /(\s*#)\b(?:else|endif|if|light|line|nowarn)\b/,
+				lookbehind: true,
+				alias: 'keyword'
+			}
+		}
+	}
+});
+Prism.languages.insertBefore('cova', 'punctuation', {
+	'computation-expression': {
+		pattern: /[_a-z]\w*(?=\s*\{)/i,
+		alias: 'keyword'
+	}
+});
+Prism.languages.insertBefore('cova', 'string', {
+	'annotation': {
+		pattern: /\[<.+?>\]/,
+		inside: {
+			'punctuation': /^\[<|>\]$/,
+			'class-name': {
+				pattern: /^\w+$|(^|;\s*)[A-Z]\w*(?=\()/,
+				lookbehind: true
+			},
+			'annotation-content': {
+				pattern: /[\s\S]+/,
+				inside: Prism.languages.cova
+			}
+		}
+	}
+});
